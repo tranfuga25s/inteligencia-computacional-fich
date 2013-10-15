8cac0035b798fc302f3c5455379319bcc930811f
tranfuga25s <tranfuga25s@gmail.com>
2013-10-15 19:12:08 -0300

    Agregada estructura para el trapecio no matricial

--- GTP3/controladordifuso.cpp
+++ GTP3/controladordifuso.cpp
@@ -6,9 +6,47 @@ ControladorDifuso::ControladorDifuso(QObject *parent) :
     _ultima_temp = 0.0;
 }
 
+void ControladorDifuso::calcularProximoPaso()
+{
+
+    // Para cada trapecio del conjunto de entradas
+    // calcular la pertenencia de cada uno y me quedo con que trapecio era
+
+
+
+}
+
 
 double ControladorDifuso::getVoltaje()
 { return 0.0; }
 
 double ControladorDifuso::getIntensidad()
 { return 0.0; }
+
+
+/*!
+ * \brief TrapecioDifuso::centroide
+ * Calcula el centroide de el trapecio considerando que se trunca en valor_entrada
+ * \param valor_y
+ * \return
+ */
+double TrapecioDifuso::centroide( double valor_y )
+{
+}
+
+
+TrapecioDifuso::TrapecioDifuso( double pos1, double pos2, double pos3, double pos4, QString nombre )
+{
+
+}
+
+
+/**
+ * @brief TrapecioDifuso::valorSalida
+ * Para un valor de entrada sobre "x" te devuleve el valor de "y" o pertenencia que le corresponda a este trapecio
+ * @param valor_entrada
+ * @return
+ */
+double TrapecioDifuso::valorSalida( double valor_entrada )
+{
+}
--- GTP3/controladordifuso.h
+++ GTP3/controladordifuso.h
@@ -3,6 +3,21 @@
 
 #include <QObject>
 
+class TrapecioDifuso {
+
+    explicit TrapecioDifuso( double pos1, double pos2, double pos3, double pos4, QString nombre );
+    double valorSalida( double valor_entrada );
+    double centroide( double valor_y );
+
+
+public:
+    double pos1;
+    double pos2;
+    double pos3;
+    double pos4;
+    QString _nombre;
+};
+
 class ControladorDifuso : public QObject
 {
     Q_OBJECT
@@ -15,7 +30,15 @@ public:
 
 private:
     double _ultima_temp;
+
+    QVector<TrapecioDifuso> _conjunto_entrada;
+
+    QVector<TrapecioDifuso> _conjunto_salida_voltaje;
+    QVector<TrapecioDifuso> _conjunto_salida_intensidad;
     
 };
+
+
+
 
 #endif // CONTROLADORDIFUSO_H
