e9e0d63e5c68ddfe79ca0ebb747c9c676304d139
tranfuga25s <tranfuga25s@gmail.com>
2013-10-29 19:03:43 -0300

    Agregado metodo de seleccion de padres

--- GTP4/EJ1/poblacion.h
+++ GTP4/EJ1/poblacion.h
@@ -7,6 +7,13 @@ template<typename T> class Poblacion : public QVector<T>
 {
 
 public:
+
+    enum MetodoSeleccion {
+        Ruleta = 1,
+        Ventana = 2,
+        Torneo = 3
+    };
+
     Poblacion();
     void setearTotal( int cantidad_total ) { _cantidad_total = cantidad_total; }
     int totalPoblacion() const { return _cantidad_total; }
@@ -14,9 +21,17 @@ public:
     void setearElitismo( bool activo ) { _elitismo = activo; }
     bool elitismo() const  { return _elitismo; }
 
+    void setearModoSeleccionPadres( Poblacion::MetodoSeleccion metodo ) { _metodo_seleccion = metodo; }
+    int modoSeleccionPadres() const { return _metodo_seleccion; }
+
+    void evaluar();
+    double mejorIndividuo() const;
+
+
 private:
     int _cantidad_total;
     bool _elitismo;
+    MetodoSeleccion _metodo_seleccion;
     
 };
 
@@ -25,5 +40,6 @@ template<typename T> Poblacion<T>::Poblacion() :
 {
     _elitismo = false;
     _cantidad_total = 0.0;
+    _metodo_seleccion = Ruleta;
 }
 #endif // POBLACION_H
