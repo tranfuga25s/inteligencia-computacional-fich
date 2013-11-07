690a0dcf9b79c1b7731dbde9128b97c63826345b
Tranfuga <tranfuga@Vanes-netbook.(none)>
2013-11-07 17:35:09 -0300

    Agregado elementos para evaluar las ventanas

--- GTPF/evaluarVentana.h
+++ GTPF/evaluarVentana.h
@@ -2,6 +2,7 @@
 #define EVALUARVENTANA_H
 
 #include "FFDW.h"
+#include "FFDWDH.h"
 #include "GenomaVentana.h"
 #include "templatealuminio.h"
 #include "TemplatePiezas.h"
@@ -17,20 +18,33 @@ double evaluar( GenomaVentana ventana ) {
         TemplateAluminio temp;
         temp.setearTipo( i );
         temp.setearLargo( ventana.at(i).largo() );
+        temp.setearCantidad( 2 );
         aluminios.append( temp );
 
         TemplateAluminio temp2;
         temp2.setearTipo( i );
         temp2.setearLargo( ventana.at(i).ancho() );
+        temp2.setearCantidad( 2 );
         aluminios.append( temp2 );
 
         // Genero los templates de Vidrios
         TemplatePiezas temp3;
         temp3.setearTipo( i );
+        temp3.setearAlto( ventana.at(i).alto() );
+        temp3.setearAncho( ventana.at(i).alto() );
+        temp3.setearCantidad( 1 );
         piezas_vidrio.append( temp3 );
-
-
     }
+
+    FFDW evaluador_aluminio;
+    evaluador_aluminio.setearTemplates( aluminios );
+
+    FFDWDH evaluador_vidrios;
+    evaluador_vidrios.setearTemplates( piezas_vidrio );
+
+    double fitness = 0.0;
+    fitnes = c1 * evaluador_aluminio.optimizar() + c2 * evaluador_vidrios.optimizar();
+    return fitness;
 }
 
 #endif // EVALUARVENTANA_H
