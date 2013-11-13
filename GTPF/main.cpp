c786925724d32a78e9fd388ad658ca3b0582a837
tranfuga25s <tranfuga25s@gmail.com>
2013-11-13 00:25:35 -0300

    bla

--- GTPF/main.cpp
+++ GTPF/main.cpp
@@ -264,7 +264,8 @@ int main(int argc, char *argv[])
     double costo_total = ventana_mejor_fitness.costoTotal( costo_vidrio, costo_aluminio );
     qDebug() << "Costo total de aluminio+vidrio: $ " << costo_total;
     qDebug() << "Costo desperdicio: $ " << mejor_fitness;
-    qDebug() << "Ganancia: $ " << costo_total - parametros.value( "costo_total", 63541.29 ).toDouble();
+    double ganancia = costo_total - parametros.value( "costo_total", 63541.29 ).toDouble();
+    qDebug() << "Ganancia: $ " << ganancia;
 
     ventana_mejor_fitness.mostrarTabla();
 
